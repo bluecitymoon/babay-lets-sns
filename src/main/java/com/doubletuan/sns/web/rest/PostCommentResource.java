b1a694e251393560491a54087809b4fe44f79632
Jerry Jiang <jerry@zhjdeMacBook-Pro.local>
2015-09-15 17:55:08 +0800

    upgrade

--- src/main/java/com/doubletuan/sns/web/rest/PostCommentResource.java
+++ src/main/java/com/doubletuan/sns/web/rest/PostCommentResource.java
@@ -1,9 +1,13 @@
 package com.doubletuan.sns.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
-import com.doubletuan.sns.domain.PostComment;
-import com.doubletuan.sns.repository.PostCommentRepository;
-import com.doubletuan.sns.web.rest.util.PaginationUtil;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -11,13 +15,18 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
+import com.codahale.metrics.annotation.Timed;
+import com.doubletuan.sns.domain.PostComment;
+import com.doubletuan.sns.repository.PostCommentRepository;
+import com.doubletuan.sns.service.PostService;
+import com.doubletuan.sns.web.rest.util.PaginationUtil;
 
 /**
  * REST controller for managing PostComment.
@@ -30,6 +39,9 @@ public class PostCommentResource {
 
     @Inject
     private PostCommentRepository postCommentRepository;
+    
+    @Resource(name="PostService")
+    private PostService postService;
 
     /**
      * POST  /postComments -> Create a new postComment.
@@ -46,6 +58,25 @@ public class PostCommentResource {
         PostComment result = postCommentRepository.save(postComment);
         return ResponseEntity.created(new URI("/api/postComments/" + postComment.getId())).body(result);
     }
+    
+    /**
+     * POST /postComments > Create a new postComment.
+     */
+	@RequestMapping(value = "/comment/{post}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<Long> comment(@RequestBody PostComment postComment, @PathVariable("post") Long postId)
+			throws URISyntaxException {
+		log.debug("REST request to save PostComment : {}, {}", postComment, postId);
+		if (postComment.getId() != null) {
+			return ResponseEntity.badRequest().header("Failure", "A new postComment cannot already have an ID")
+					.body(null);
+		}
+
+		PostComment savedPostComment = postService.commentPost(postComment, postId);
+
+		return ResponseEntity.ok(savedPostComment.getId());
+	}
+
 
     /**
      * PUT  /postComments -> Updates an existing postComment.
